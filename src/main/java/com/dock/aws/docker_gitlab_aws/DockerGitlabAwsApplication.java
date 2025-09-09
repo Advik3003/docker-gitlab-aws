f462fcca10f83e7db7941d38945a8c46224cd355
Advik3003 <Advik3003@gmail.com>
2025-09-09 14:02:13 +0530

    Initial commit: Spring Boot Hello World

---
+++ src/main/java/com/dock/aws/docker_gitlab_aws/DockerGitlabAwsApplication.java
@@ -0,0 +1,13 @@
+package com.dock.aws.docker_gitlab_aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerGitlabAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerGitlabAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dock/aws/docker_gitlab_aws/DockerGitlabAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dock.aws.docker_gitlab_aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerGitlabAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
